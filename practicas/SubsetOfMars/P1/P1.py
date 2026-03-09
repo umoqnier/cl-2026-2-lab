753f9a26e9b78272697f9cafcf1d46d3f6b11f03
Eunice Contreras Ortiz <eunicecontrerasortiz@gmail.com>
2026-03-09 14:32:51 -0600

    Descarga del diccionario IPA

--- practicas/SubsetOfMars/P1/P1.py
+++ practicas/SubsetOfMars/P1/P1.py
@@ -7,4 +7,36 @@
 #       format_name: percent
 #       format_version: '1.3'
 #       jupytext_version: 1.19.1
+#   kernelspec:
+#     display_name: P1 (3.12.13)
+#     language: python
+#     name: python3
 # ---
+
+# %% [markdown]
+# # Práctica 1 - Lingüística Computacional
+#
+
+# %% [markdown]
+# # Fonética
+
+# %%
+from collections import defaultdict
+
+import pandas as pd
+import requests
+
+# %%
+IPA_URL = "https://raw.githubusercontent.com/open-dict-data/ipa-dict/master/data/{lang}.txt"
+
+# %%
+response = requests.get(IPA_URL.format(lang="es_MX"))
+
+# %%
+ipa_list = response.text.split("\n")
+
+# %%
+ipa_list[0].split("\t")
+
+# %% [markdown]
+# # Morfología
